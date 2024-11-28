c8653e4a87a7b1d2f5c0b8361dc97980cb352636
Przemek <przemek12wiewiora@gmail.com>
2024-11-28 23:09:04 +0100

    Add sdl3

--- Engine/Source/Private/Engine.cpp
+++ Engine/Source/Private/Engine.cpp
@@ -2,11 +2,19 @@
 //
 
 #include "Engine.h"
+#include "SDL3/SDL.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+	SDL_Window* Window = SDL_CreateWindow("tt", 100, 100, SDL_WINDOW_RESIZABLE);
+	if (Window != nullptr)
+	{
+		cout << "Window created." << endl;
+	}
+
 	return 0;
 }
---
+++ Scripts/Build/BuildSDLs.sh
@@ -0,0 +1,13 @@
+cd ..
+cd ..
+cd ThirdParty
+
+cd SDL
+
+mkdir build
+cd build
+cmake .. -G "YourGeneratorHere"
+cmake --build .
+
+cd ..
+
