c2ad91f2452e05fd354510b2e32c52dc384ea366
Przemek <przemek12wiewiora@gmail.com>
2024-12-04 21:01:16 +0100

    Add full libraries

--- Engine/Source/Public/Core/Includes/FullThirdParty.h
+++ Engine/Source/Public/Core/Includes/FullThirdParty.h
@@ -4,6 +4,6 @@
 
 // ThirdParty includes
 #include "SDL3/SDL.h"
-#include "SDL3/SDL_ttf.h"
-#include "SDL3/SDL_mixer.h"
-#undef main // Required as SDL defines it's own main.
+#include "SDL3_ttf/SDL_ttf.h"
+#include "SDL3_mixer/SDL_mixer.h"
+//#undef main // Required as SDL defines it's own main.
--- PreBuiltLibs/Windows/SDL_mixer_build/external/wavpack/CMakeFiles/CheckASM/result.cmake
+++
@@ -1,2 +0,0 @@
-set(CMAKE_ASM_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.36.32532/bin/Hostx64/x64/cl.exe")
-
