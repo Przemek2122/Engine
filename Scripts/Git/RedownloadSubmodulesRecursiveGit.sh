fd0d397728d1969845fd786d29b8d186a376657e
Przemek <przemek12wiewiora@gmail.com>
2025-02-26 20:44:39 +0100

    update

--- Scripts/Git/RedownloadSubmodulesRecursiveGit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ..
-cd ..
-git submodule deinit --all
-git submodule update --init --force --remote
--- Scripts/Git/UpdateSubmodulesRecursiveGit.sh
+++ Scripts/Git/UpdateSubmodulesRecursiveGit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ..
 cd ..
+git submodule update --remote --merge
 git submodule update --init --recursive
+git commit -m "Update submodule to version v1.2.3"
