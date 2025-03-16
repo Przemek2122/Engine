f363685f27e91519f93a1b19b9fdcb8d8a6270f9
Przemek <przemek12wiewiora@gmail.com>
2025-03-16 09:44:00 +0100

    Update EventHandlerShared.h

--- Engine/Source/Public/Core/Input/EventHandlerShared.h
+++ Engine/Source/Public/Core/Input/EventHandlerShared.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "CoreMinimal.h"
+
+// @TODO: EInputState should be uint8
 /** This class is used to tell type of input, if it is pressed or released. */
 enum class ENGINE_API EInputState
 {
 	NOT_PRESSED = 0,
-	PRESS = 1,
-	RELEASE = 2,
+	PRESS,
+	RELEASE,
 	ENUM_MAX
 };
